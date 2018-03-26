e5c19aeec9f9fbf7d9f392ed83f881942a6fee36
WarrG3X <warrier.abhishek@gmail.com>
2018-03-26 10:42:09 +0530

    Ex3 Completed

--- ex3.cxx
+++ ex3.cxx
@@ -6,6 +6,24 @@ std::map<int,std::string> powerset;
 std::map<std::string,int> powerset2;
 std::vector<char> symbols;
 
+void Update(std::vector<char> &vec, char a){
+	if(std::find(vec.begin(),vec.end(),a)==vec.end())
+		vec.push_back(a);
+}
+
+std::vector<std::string> Parse(std::string prod){
+
+	std::istringstream parser(prod);
+	std::string p;
+	std::vector<std::string> prods;
+
+	while(std::getline(parser, p, '/'))
+    			prods.push_back(p);
+
+	return prods;
+
+}
+
 
 void findPowerSet(std::vector<int> const S, int n)
 {
@@ -34,23 +52,40 @@ public:
 
 	State(std::string name){
 		this->name = name;
+		trans = "";
 	}
 
 	void add_trans(std::string trans){
-		this->trans = trans;
+		this->trans += trans;
 	}
 
 };
 
-std::string get_trans(char S, char sym){
+std::string get_trans(std::string S, char sym,std::vector<std::string> transition){
 
-	std::string trans;
+	std::string trans = "";
+	for(char s:S){
+		for(std::string t: transition){
+			if (t[0]==s && t[1]==sym)
+				trans.push_back(t[2]);
+		}
+	}
+
+
+	//std::cout<<S<<">"<<sym<<"> "<<trans<<"\n";
+	if(trans=="")
+		return "0/";
+	else
+		return std::to_string(powerset2[trans])+"/";
+
+
 }
 
 int main()
 {
 	int n = 3;
 	int N = pow(2, n);
+	int start = 1;
 
 	std::vector<int> s;
 	for(char i=48;i<48+n;i++)
@@ -73,17 +108,50 @@ int main()
 	std::cout<<"NFA,\n";
 	for(auto t:transitions){
 		std::cout<<t[0]<<" -- "<<t[1]<<" --> "<<t[2]<<"\n";
-		symbols.push_back(t[1]);
+		Update(symbols,t[1]);
+	}
+
+
+	for(char sym:symbols){
+
+		for(State &st:states){
+			
+			st.add_trans(get_trans(st.name,sym,transitions));
+
+		}
 	}
 
 
 
+	std::cout<<"\n\nIntermediate Form from Subset Construction,\n";
+	for(int i=0;i<N;i++)
+		std::cout<<i<<"->"<<states[i].trans<<"\n";
+
+	std::vector<int> final;
+	std::deque<int> buffer;
+	buffer.push_back(start);
+	while(!buffer.empty()){
+		int b = buffer.front();
+		buffer.pop_front();
+		if(std::find(final.begin(),final.end(),b)==final.end()){
+			final.push_back(b);
+			std::vector<std::string> trans = Parse(states[b].trans);
+			for(auto t:trans){
+				buffer.push_back(std::stoi(t));
+			}
+		}
+	}
+
+	std::cout<<"\n\nFinal DFA,\n";
+
+	for(auto i:final){
+		for(int j=0;j<symbols.size();j++){
+			std::vector<std::string> trans = Parse(states[i].trans);
+			std::cout<<i<<" -- "<<symbols[j]<<" --> "<<trans[j]<<"\n";
+		}
+	}
 
 
-
-
-	
-
-
+return 0;
 
 }
